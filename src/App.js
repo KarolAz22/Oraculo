a9b5d6673e3fb7f52d012cf8b480b177f9164b1b
Karol Azevedo <karolina.azevedo@ufvjm.edu.br>
2025-07-16 11:15:35 -0300

    Adicionando pagina planos

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/homePage';
 import Stores from './Pages/Stores/storesPage';
-//import Plans from './pages/Plans';
-//import About from './pages/About';
-//import Contact from './pages/Contact';
+import Plans from './Pages/Plans/plansPage';
+//import About from './Pages/About';
+//import Contact from './Pages/Contact';
 
 function App() {
   return (
@@ -12,8 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/stores" element={<Stores />} />
-        {/*<Route path="/plans" element={<Plans />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/plans" element={<Plans />} />
+        {/*<Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />*/}
       </Routes>
     </Router>
---
+++ src/Pages/Plans/plansPage.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import "./stylePlans.css";
+import Footer from "../../Componentes/Footer/footer";
+import Navbar from "../../Componentes/Navbar/navbar";
+import backgroundImagePlans from "../../Componentes/Navbar/imgPlans.svg";
+
+const Plans = () => {
+  return (
+    <div>
+      <div className="hero-section">
+        <Navbar backgroundImage={backgroundImagePlans} />
+        <div className="conteiner-palavra">
+          <div className="estatico2">Parceiros</div>
+        </div>
+        <div>
+          <span className="frase">
+            Venha fazer parte da nossa <br /> plataforma{" "}
+          </span>
+        </div>
+      </div>
+      <div className="planos-container">
+        <h2 className="titulo">
+          Para ser um parceiro entre em contato conosco pelo <br />
+          e-mail e escolha seu plano
+        </h2>
+
+        <div className="email-box">emailOraculo@email.com</div>
+
+        <div className="plano-card">
+          <h3 className="nome-plano">Plano mensal</h3>
+          <p className="descricao">
+            Com o plano mensal você tem seu anúncio disponível na <br />
+            plataforma por 30 dias pelo valor de
+          </p>
+          <p className="preco">R$ 300,00</p>
+        </div>
+
+        <div className="plano-card">
+          <h3 className="nome-plano">Plano semestral</h3>
+          <p className="descricao">
+            Com o plano semestral você tem seu anúncio disponível na <br />
+            plataforma por 6 meses pelo valor de
+          </p>
+          <p className="preco">R$ 1.740,00</p>
+        </div>
+
+        <div className="plano-card">
+          <h3 className="nome-plano">Plano anual</h3>
+          <p className="descricao">
+            Com o plano anual você tem seu anúncio disponível na <br />
+            plataforma por 1 ano pelo valor de
+          </p>
+          <p className="preco">R$ 3.360,00</p>
+        </div>
+
+        <div className="email-box">emailOraculo@email.com</div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Plans;
